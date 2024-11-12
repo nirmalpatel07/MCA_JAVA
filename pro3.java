61f1b72b986dec2ab3937a3ddef11ca9af594da9
Nirmal Vaghasiya <138444412+nirmalpatel07@users.noreply.github.com>
2024-11-12 11:35:12 +0000

    palindrone number

---
+++ pro3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class pro3 {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a integer number : ");
+        int org_num = sc.nextInt();
+        int num = org_num;
+        int rev_num = 0;
+
+        while(num>0){
+            int last_digit = num % 10;
+            rev_num = rev_num*10 + last_digit;
+            num = num/10;
+        }
+        
+        if(org_num == rev_num){
+            System.out.println("Number is Palindrome");
+        }else{
+            System.out.println("Number is not Palindrone");
+        }
+
+    }
+}
